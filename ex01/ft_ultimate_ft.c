3ba135405e6a7ff6fe9cb5eb36a092c457c36265
Bongani Christopher Simelane <bsimelan@c5r1s9.wethinkcode.co.za>
2020-06-24 17:14:12 +0200

    ex01

---
+++ ex01/ft_ultimate_ft.c
@@ -0,0 +1,35 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_ultimate_ft.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: bsimelan <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/06/24 10:41:24 by bsimelan          #+#    #+#             */
+/*   Updated: 2020/06/24 10:54:09 by bsimelan         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+int    a;
+
+int    *ptr;
+    int    **ptrb;
+    int    ***ptrc;
+    int    ****ptrd;
+    int    *****ptre;
+    int    ******ptrf;
+    int    *******nbrg;
+    int    ********nbrh;
+    int    *********nbri;
+    a = 0;
+    ptr = &a;
+    nbrb= &nbr;
+    nbrc = &nbrb;
+    nbrd = &nbrc;
+    nbre = &nbrd;
+    nbrf = &nbre;
+    nbrg = &nbrf;
+    nbrh = &nbrg;
+    nbr = &nbrh;
+
+    ft_ultimate_ft(*********nbr);
